590633ce3c58d5aaf5d50e962b251268fd7ac59f
OIV06 <117694885+OIV06@users.noreply.github.com>
2024-03-06 14:44:43 -0800

    Update wish.c

--- wish.c
+++ wish.c
@@ -6,3 +6,17 @@
 #include <sys/wait.h>
 #include <ctype.h>
 #include <fcntl.h>
+
+int chng_directory(char *path){
+
+    if(path == NULL || strcmp(path, "~") == 0){
+        path = getenv("HOME");
+}
+
+    if (chdir(path)!=0){
+        perror("cd");
+        return -1;
+
+    }
+    return O;
+}
